09a374206c27142c51970fbd4a8651c295b2a7b1
vienhuynhxuan <43909447+vienhuynhxuan@users.noreply.github.com>
2019-06-20 16:38:06 +0700

    remove console.log in file

--- server/controllers/accounts/whoami.controller.js
+++ server/controllers/accounts/whoami.controller.js
@@ -2,7 +2,6 @@
 /* eslint-disable consistent-return */
 const whoami = async (req, res) => {
   const user = res.locals.user;
-  console.log('--whoami: ', user);
   if (!user) {
     return res.send({ error: true, message: 'Invalid whoami.' });
   }
